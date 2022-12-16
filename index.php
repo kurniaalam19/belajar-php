d92a32be84e479a035bd6a3c3b13a825d697ee25
Alam Kurnia Al Fatah <alamreal19@gmail.com>
2022-12-16 14:56:41 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+    
+    echo "Hello World"; 
+
+    $nama = "Alam" ;
+    $umur = 20;
+
+    echo "Nama saya <strong>$nama</strong>, umur saya <strong>$umur</strong> tahun.<br>";
+
+    $namaAdik = "Kurnia";
+    $umurAdik = 18;
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, umur adik saya <strong>$umurAdik</strong> tahun";
+
+    
+    $SelisihUmur = $umur - $umurAdik;
+    echo "Selisih usia saya dan adik saya adalah $SelisihUmur tahun.<br>"
+    
+
+?>
